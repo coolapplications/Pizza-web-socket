565297a6a11483e4e48536612ee81c83b64153c9
Andrés Alvarez <amauricio.alvarez@udea.edu.co>
2018-09-15 13:35:27 -0500

    Update JSON function implemented

--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@ let app = express();
 let http = createServer(app);
 let io = socketIO(http);
 
-const orders = [];
+let orders = [];
 
 io.on("connection", socket => {
   console.log("user connected");
@@ -19,6 +19,18 @@ io.on("connection", socket => {
 
   socket.on("add-order", message => {
     orders.push(JSON.parse(message));
+    io.emit("order", JSON.stringify(orders));
+  });
+  socket.on("edit-order", message => {
+    let partialOrder = JSON.parse(message);
+    let order = orders.find(
+      item => partialOrder.orderNumber === item.orderNumber
+    );
+    let index = orders.findIndex(
+      item => partialOrder.orderNumber === item.orderNumber
+    );
+    order = { ...order, ...partialOrder };
+    orders = [...orders.slice(0, index), order, ...orders.slice(index + 1)];
     io.emit("order", JSON.stringify(orders));
   });
 });
